20e3265f680e8ec4c22b75200ff06cdda2e43cbe
Mike C <carolanm818@gmail.com>
2025-02-25 19:35:37 -0500

    Began array algorithm

--- Project2/myjs_web.js
+++ Project2/myjs_web.js
@@ -1,10 +1,19 @@
-function grow(){
-    if(onmousedown() === true){
-        docu
+document.addEventListener("DOMContentLoaded", function(){
+    const Titles = []
+    const Descs = []
+    document.querySelector("form").onsubmit = function(){
+       let TaskTitle = document.querySelector("#TaskTitle").value
+        let TaskDesc = document.querySelector("#TaskDesc").value
 
+        Titles.length = Titles.length + 1;
+       Descs.length = Titles.length;
+        Titles[Titles.length - 1] = TaskTitle;
+        Descs[Descs.length - 1] = TaskDesc;
+        for(let i = 0; i < Titles.length; i++){
+
+        }
+        return false;
     }
-}
+    })
 
-function returnToIndex(){
-    document.
-}
+

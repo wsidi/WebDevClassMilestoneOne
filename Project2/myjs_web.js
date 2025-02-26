9b533bbc2fe349e6d93a74a6447ef35d7d31a4c4
Mike C <carolanm818@gmail.com>
2025-02-26 16:37:36 -0500

    Algo works

--- Project2/myjs_web.js
+++ Project2/myjs_web.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
     const Titles = []
     const Descs = []
-    document.querySelector("form").onsubmit = function(){
+    document.querySelector("#create").onsubmit = function(){
        let TaskTitle = document.querySelector("#TaskTitle").value
         let TaskDesc = document.querySelector("#TaskDesc").value
-
         Titles.length = Titles.length + 1;
        Descs.length = Titles.length;
         Titles[Titles.length - 1] = TaskTitle;
         Descs[Descs.length - 1] = TaskDesc;
+        const li = document.createElement("li");
         for(let i = 0; i < Titles.length; i++){
+                    let new_task_html = `
+                                                   <span><h3> ${Titles[i]} </h3><br></span>
+                                                    <span>  ${Descs[i]} </span>
+                                                  `;
+            li.innerHTML = new_task_html;
 
         }
+        document.querySelector("#ExistT").append(li);
+        document.querySelector("#TaskTitle").value = "";
+        document.querySelector("#TaskDesc").value = "";
         return false;
     }
     })
